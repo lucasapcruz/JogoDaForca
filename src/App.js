9775bb8622efd93f782f60297691109b07f597af
Lucas Cruz <lucas.apcruz@gmail.com>
2022-09-24 20:33:13 -0300

    Change page title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import palavras from "./palavras"
+
 export default function App(){
     return(
         <>
             <div>
                 <div className="hangman">
-                    <img src="assets/forca0.png"/>
+                    <img src="assets/forca0.png" data-identifier="game-image"/>
                 </div>
                 <div className="word-utilities">
-
+                    <button data-identifier="choose-word" >Escolher Palavra</button>
                 </div>
             </div>
             <div className="keyboard">
 
             </div>
             <div className="guess">
-
+                <label for="guess">Já sei a palavra</label>
+                <input type="text" name="guess" data-identifier="type-guess"></input>
+                <button data-identifier="guess-button"></button>
             </div>
         </>
     )
