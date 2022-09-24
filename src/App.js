be647c56fe143595482f4e7015bbacc30fe3ca80
Lucas Cruz <lucas.apcruz@gmail.com>
2022-09-24 14:10:27 -0300

    Revert start and build scripts to legacy openssl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 export default function App(){
     return(
         <>
+            <div>
+                <div className="hangman">
+                    <img src="assets/forca0.png"/>
+                </div>
+                <div className="word-utilities">
+
+                </div>
+            </div>
+            <div className="keyboard">
+
+            </div>
+            <div className="guess">
+
+            </div>
         </>
     )
 }
