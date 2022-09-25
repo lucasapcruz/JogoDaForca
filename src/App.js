f38c6c621dd6c91f9b9b0bcb0ef831aba82d98c3
Lucas Cruz <lucas.apcruz@gmail.com>
2022-09-24 21:52:08 -0300

    Add chooseWord feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
+import { useState } from "react"
 import palavras from "./palavras"
 
+
 export default function App(){
+
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+    const [word, setWord] = useState([])
+
+    
+    function comparator() {
+        return Math.random() - 0.5
+    }
+
+    function chooseWord(){
+        palavras.sort(comparator)
+        setWord(palavras[0].split(""))
+    }
+
     return(
-        <>
-            <div>
+        <div className="content">
+            <div className="container">
                 <div className="hangman">
                     <img src="assets/forca0.png" data-identifier="game-image"/>
                 </div>
                 <div className="word-utilities">
-                    <button data-identifier="choose-word" >Escolher Palavra</button>
+                    <button data-identifier="choose-word" onClick={() => chooseWord()}>Escolher Palavra</button>
+                    <div className="word">
+                        {word.map((character) => <span>{character}</span>)}
+                    </div>
                 </div>
             </div>
             <div className="keyboard">
-
+                {alphabet.map((letter) => <button className="letter" data-identifier="letter">{letter}</button>)}
             </div>
             <div className="guess">
-                <label for="guess">Já sei a palavra</label>
+                <label htmlFor="guess">Já sei a palavra</label>
                 <input type="text" name="guess" data-identifier="type-guess"></input>
-                <button data-identifier="guess-button"></button>
+                <button data-identifier="guess-button">Chutar</button>
             </div>
-        </>
+        </div>
     )
 }
