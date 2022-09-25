bd74cb8f9738792aa1e3b5d9849db806e772f94e
Lucas Cruz <lucas.apcruz@gmail.com>
2022-09-25 01:29:08 -0300

    Add character guessing logic

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import palavras from "./palavras"
 
 
-export default function App(){
+export default function App() {
 
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
@@ -11,40 +11,50 @@ export default function App(){
     const [wordGuess, setWordGuess] = useState([])
     const [hangmanState, setHangmanState] = useState(0)
 
-    
+
     function comparator() {
         return Math.random() - 0.5
     }
 
-    function chooseWord(){
+    function chooseWord() {
         palavras.sort(comparator)
         setWord(palavras[0].split(""))
         let underscoreWord = "_".repeat(palavras[0].length)
         setWordGuess(underscoreWord.split(""))
     }
 
-    function characterGuess(character){
-        if(word.includes(character)){
-
-        }else{
-            setHangmanState(hangmanState+1)
+    function guessCharacter(character) {
+        if (word.includes(character)) {
+            const updatedGuessWord = word.map((c, index) => c===character?c:wordGuess[index])
+            setWordGuess(updatedGuessWord)
+        } else {
+            setHangmanState(hangmanState + 1)
+            checkDefeat()
         }
 
     }
 
-    function checkWin(){
+    function guessWord(){  
+
 
     }
 
-    function checkDefeat(){
+    function checkWin() {
 
     }
 
-    return(
+    function checkDefeat() {
+        if(hangmanState+1===6){
+            setWordGuess(word)
+        }
+
+    }
+
+    return (
         <div className="content">
             <div className="container">
                 <div className="hangman">
-                    <img src="assets/forca0.png" data-identifier="game-image"/>
+                    <img src="assets/forca0.png" data-identifier="game-image" />
                 </div>
                 <div className="word-utilities">
                     <button data-identifier="choose-word" onClick={() => chooseWord()}>Escolher Palavra</button>
@@ -54,7 +64,7 @@ export default function App(){
                 </div>
             </div>
             <div className="keyboard">
-                {alphabet.map((letter) => <button className="letter" data-identifier="letter" onClick={(event) => characterGuess(event.target.textContent)}>{letter}</button>)}
+                {alphabet.map((letter) => <button className="letter" data-identifier="letter" onClick={(event) => guessCharacter(event.target.textContent)}>{letter}</button>)}
             </div>
             <div className="guess">
                 <label htmlFor="guess">Já sei a palavra</label>
